e744a30118d25835b611ab1890482f7bc95a4cb1
James Newman <jamesmnewman1@gmail.com>
2016-02-19 23:26:14 -0800

    just a route

--- chap5/example16/www/js/app.js
+++ chap5/example16/www/js/app.js
@@ -22,3 +22,20 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('home', {
+    url: '/home',
+    abstract: true,
+   // templateUrl: ''
+  })
+
+}
