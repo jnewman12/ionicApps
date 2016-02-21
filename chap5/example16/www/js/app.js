0c81402fa09a34d0ba4d76f8f18f101e2d883682
James Newman <jamesmnewman1@gmail.com>
2016-02-20 18:28:29 -0800

    run method

--- chap5/example16/www/js/app.js
+++ chap5/example16/www/js/app.js
@@ -5,22 +5,48 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//       // for form inputs)
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+//       // Don't remove this line unless you know what you are doing. It stops the viewport
+//       // from snapping when text inputs are focused. Ionic handles this internally for
+//       // a much nicer keyboard experience.
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//     if(window.StatusBar) {
+//       StatusBar.styleDefault();
+//     }
+//   });
+// })
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+  var alertOnBackPress = localStorage.getItem('alertOnBackPress');
+  var hardwareBackButtonHandler = function() {
+    console.log('Hardware back button pressed');
+    // do more interesting things here
+  }
+  function manageBackPressEvent(alertOnBackPress) {
+    if (alertOnBackPress) {
+      $ionicPlatform.onHardwareBackButton(hardwareBackButtonHandler);
+    } else {
+     $ionicPlatform.offHardwareBackButton(hardwareBackButtonHandler);
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+  }
+  $ionicPlatform.offHardwareBackButton(hardwareBackButt
+  // when the app boots up
+  manageBackPressEvent(alertOnBackPress);
+  // later in the code/controller when you let
+  // the user update the setting
+  function updateSettings(alertOnBackPressModified) {
+      localStorage.setItem('alertOnBackPress',alertOnBackPressModified);
+          manageBackPressEvent(alertOnBackPressModified)
+      }
+  }); 
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
