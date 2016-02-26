b0674c359b84be0e50ee613351c137f31241a8da
James Newman <jamesmnewman1@gmail.com>
2016-02-25 21:52:32 -0800

    intersting view lifecycle

--- chap5/example18/www/js/app.js
+++ chap5/example18/www/js/app.js
@@ -5,23 +5,52 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//       // for form inputs)
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+//       // Don't remove this line unless you know what you are doing. It stops the viewport
+//       // from snapping when text inputs are focused. Ionic handles this internally for
+//       // a much nicer keyboard experience.
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//     if(window.StatusBar) {
+//       StatusBar.styleDefault();
+//     }
+//   });
+// })
+
+.run(function($ionicPlatform, $rootScope) {
+  // View Life Cycle
+ $rootScope.$on('$ionicView.loaded', function(event, view) {
+    console.log('Loaded..', view.stateName);
   });
-})
+  $rootScope.$on('$ionicView.beforeEnter', function(event, view) {
+    console.log('Before Enter..', view.stateName);
+  });
+  $rootScope.$on('$ionicView.afterEnter', function(event, view) {
+    console.log('After Enter..', view.stateName);
+  });
+  $rootScope.$on('$ionicView.enter', function(event, view) {
+    console.log('Entered..', view.stateName);
+  });
+  $rootScope.$on('$ionicView.leave', function(event, view) {
+    console.log('Left..', view.stateName);
+  });
+  $rootScope.$on('$ionicView.beforeLeave', function(event, view){
+    console.log('Before Leave..', view.stateName);
+  });
+  $rootScope.$on('$ionicView.afterLeave', function(event, view){
+    console.log('After Leave..', view.stateName);
+  });
+  $rootScope.$on('$ionicView.unloaded', function(event, view) {
+    console.log('View unloaded..', view.stateName);
+  });
+})    
+
 
 
 .config(function($stateProvider, $urlRouterProvider) {
