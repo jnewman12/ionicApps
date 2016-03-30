8bfe208f8320ceb7469813e8ac55a4abd5822986
James Newman <jamesmnewman1@gmail.com>
2016-03-29 23:45:44 -0700

    spacing

--- chap6/book-store/www/js/app.js
+++ chap6/book-store/www/js/app.js
@@ -140,30 +140,30 @@ angular.module('BookStoreApp', ['ionic', 'BookStoreApp.controllers', 'AuthFactor
 }])
 
 .factory('AuthFactory', ['LSFactory', function(LSFactory) {
-    var userKey = 'user';
-    var tokenKey = 'token';
-    var AuthAPI = {
-      isLoggedIn: function() {
-         return this.getUser() === null ? false : true;
-      },
-      getUser: function() {
-         return LSFactory.get(userKey);
-      },
-      setUser: function(user) {
-        return LSFactory.set(userKey, user);
-      },
-      getToken: function() {
-        return LSFactory.get(tokenKey);
-      },
-      setToken: function(token) {
-        return LSFactory.set(tokenKey, token);
-      },
-      deleteAuth: function() {
-        LSFactory.delete(userKey);
-        LSFactory.delete(tokenKey);
-      } 
-    };
-    return AuthAPI;
+  var userKey = 'user';
+  var tokenKey = 'token';
+  var AuthAPI = {
+    isLoggedIn: function() {
+       return this.getUser() === null ? false : true;
+    },
+    getUser: function() {
+       return LSFactory.get(userKey);
+    },
+    setUser: function(user) {
+      return LSFactory.set(userKey, user);
+    },
+    getToken: function() {
+      return LSFactory.get(tokenKey);
+    },
+    setToken: function(token) {
+      return LSFactory.set(tokenKey, token);
+    },
+    deleteAuth: function() {
+      LSFactory.delete(userKey);
+      LSFactory.delete(tokenKey);
+    } 
+  };
+  return AuthAPI;
 }])
 
 
